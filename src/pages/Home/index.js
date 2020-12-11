98e6ea2b86e73faed4e3e7a6048b002b9ee0e974
Lucas <lucaskmboulhosa@gmail.com>
2020-12-11 21:22:42 +0000

    started api connection, formatted price function

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,115 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
 
+import { formatPrice } from '../../utility/format';
+import api from '../../services/api';
 // import { Container } from './styles';
 
-function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tenis Legal</strong>
-        <span>R$ 129,20</span>
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+  }
 
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
+  async componentDidMount() {
+    const response = await api.get('products');
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
+    this.setState({
+      products: data,
+    });
+  }
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tenis Legal</strong>
-        <span>R$ 129,20</span>
+  render() {
+    const { products } = this.state;
 
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong> {product.title} </strong>
+            <span> {product.priceFormatted} </span>
 
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
+            <button type="button">
+              <div>
+                <MdShoppingCart size={16} color="#fff" /> 3
+              </div>
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tenis Legal</strong>
-        <span>R$ 129,20</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tenis Legal</strong>
-        <span>R$ 129,20</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tenis Legal</strong>
-        <span>R$ 129,20</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
-          alt="Tenis"
-        />
-        <strong>Tenis Legal</strong>
-        <span>R$ 129,20</span>
-
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#fff" /> 3
-          </div>
-
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span> ADICIONAR AO CARRINHO </span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
 
 export default Home;
